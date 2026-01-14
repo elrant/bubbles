2dc9cf8019b5154cfa62c9edd3050ca710d2fa66
Iris Nkrichronos <irisnk@elrant.team>
2026-01-14 18:34:17 +0100

    improve button labeling in RosterFrame

--- src/main/java/team/elrant/bubbles/frames/ChatFrame.java
+++ src/main/java/team/elrant/bubbles/frames/ChatFrame.java
@@ -25,5 +25,6 @@ public final class ChatFrame {
   }
 
   private ChatFrame() {
+    
   }
 }
--- src/main/java/team/elrant/bubbles/frames/RosterFrame.java
+++ src/main/java/team/elrant/bubbles/frames/RosterFrame.java
@@ -2,14 +2,24 @@ package team.elrant.bubbles.frames;
 
 import cc.lunary.swingify.SwingItem;
 import net.miginfocom.swing.MigLayout;
+import org.jivesoftware.smack.roster.RosterEntry;
 import team.elrant.bubbles.Bubbles;
 import team.elrant.bubbles.event.LoggedInEvent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class RosterFrame {
   public static SwingItem<JFrame> ITEM;
+
+
+  private static String rosterLabel(RosterEntry entry) {
+    return Objects.requireNonNullElse(
+        entry.getName(),
+        entry.getJid().toString()
+    );
+  }
 
   static {
     var builder = new SwingItem.Builder<>(JFrame::new)
@@ -23,14 +33,13 @@ public class RosterFrame {
     Bubbles.get().events().register(LoggedInEvent.class, event -> {
       for (var entry : event.user().getRoster().getEntries()) {
         if (entry.getName() != null) {
-          rosterPanel.add(new SwingItem.Builder<>(() -> new JButton(entry.getName())).build());
-        } else {
-          rosterPanel.add(new SwingItem.Builder<>(() -> new JButton(entry.getJid().toString())).build());
+          rosterPanel.add(new SwingItem.Builder<>(() -> new JButton(rosterLabel(entry))).build()
+          );
         }
-        
       }
+
+      ITEM = builder.add(rosterPanel.build()).build();
     });
 
-    ITEM = builder.add(rosterPanel.build()).build();
   }
 }
